9712a671859dbc6ee0da6140ccc1991229551313
Arzoo Jangra <aaa.jangra123@gmail.com>
2024-03-25 20:38:33 +0530

    bottom rights

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import WeatherWidget from "./Components/WeatherWidget";
 function App() {
   const [searchLocation, setSearchLocation] = useState(null);
   return (
-    <div className="bg-image bg-cover bg-fixed h-screen w-full">
+    <div className="bg-image bg-cover bg-fixed h-screen w-full relative">
       <div className="flex justify-center items-center m-auto w-full">
         {!searchLocation && (
           <CurrentLocation setSearchLocation={setSearchLocation} />
@@ -16,6 +16,10 @@ function App() {
             setSearchLocation={setSearchLocation}
           />
         )}
+      </div>
+      <div className="fixed inset-x-0 bottom-0 font-semibold text-center backdrop-blur-lg">
+        © 2024 Weather-App | All rights reserved | Designed and developed by{" "}
+        <b>Arzoo Jangra</b>
       </div>
     </div>
   );
